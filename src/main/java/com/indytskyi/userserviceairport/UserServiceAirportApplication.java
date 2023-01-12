999917ce0ef4f3325fd2a810270c9826366a1f5c
Indytskyi <ind.artem2003@gmail.com>
2023-01-12 17:47:20 +0200

    Initialization and add dependencies

---
+++ src/main/java/com/indytskyi/userserviceairport/UserServiceAirportApplication.java
@@ -0,0 +1,13 @@
+package com.indytskyi.userserviceairport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceAirportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceAirportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indytskyi/userserviceairport/UserServiceAirportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indytskyi.userserviceairport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceAirportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
