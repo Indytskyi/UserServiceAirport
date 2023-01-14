a0838620f44ffd708f9f107dada3e521d016d969
Indytskyi Artem <ind.artem2003@gmail.com>
2023-01-14 18:19:12 +0200

    create two tables with flyway

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -1,13 +1,14 @@
-CREATE TABLE user (
+CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     email varchar NOT NULL UNIQUE,
-    password varchar NOT NULL
+    "password" varchar NOT NULL
 );
 
-CREATE TABLE passenger (
-    name varchar NOT NULL,
+CREATE TABLE passengers (
+    id BIGSERIAL PRIMARY KEY REFERENCES users(id),
+    "name" varchar NOT NULL,
     surname varchar NOT NULL,
     data_birth DATE NOT NULL,
     gender varchar NOT NULL,
-    photo varchar
+    photo varchar NOT NULL
 );
