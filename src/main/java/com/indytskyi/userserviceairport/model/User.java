c5536e6794029cc4fd28418b51af98731795fde7
Indytskyi Artem <ind.artem2003@gmail.com>
2023-01-16 19:03:33 +0200

    configurate jwt and spring security

--- src/main/java/com/indytskyi/userserviceairport/model/User.java
+++ src/main/java/com/indytskyi/userserviceairport/model/User.java
@@ -7,13 +7,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Cascade;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,5 +38,34 @@ public class User {
     public void setPassenger(Passenger passenger) {
         this.passenger = passenger;
         passenger.setUser(this);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true ;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
