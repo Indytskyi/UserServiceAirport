147e716658741988fb832e7cf8838c988ad1c59b
Indytskyi Artem <ind.artem2003@gmail.com>
2023-01-16 18:46:22 +0200

    create DTOs for registgration and authentication and also cofigurate UserDetails

---
+++ src/main/java/com/indytskyi/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.indytskyi.dto;
+
+import com.indytskyi.userserviceairport.model.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+  private String email;
+  private String password;
+  private String firstName;
+  private String lastName;
+  private Gender gender;
+  private Date dateOfBirth;
+  private String photoLink;
+
+
+}
